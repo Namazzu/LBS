87a07b677fc05a1715281e0432f5f3d8a23ffd16
ofdev <olivefdev@gmail.com>
2021-08-23 09:45:48 +0200

    integrate burger menu to nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,18 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
+import { Burger } from './Burger'
+import { Menu } from './Menu'
+
 import style from '../styles/Navbar.module.css'
+
 export function Navbar() {
+	const [open, setOpen] = useState(false)
+	const [currentPage, setCurrentPage] = useState()
+
+	useEffect(() => {
+		setCurrentPage(window.location.pathname)
+	}, [open])
+
 	return (
 		<div className={style.container}>
 			<h2 className={style.logo}>
@@ -8,18 +20,14 @@ export function Navbar() {
 					<a>LBS</a>
 				</Link>
 			</h2>
-			<ul className={style.nav_items_container}>
-				<li className={style.nav_item}>
-					<Link href='/works'>
-						<a>Réalisation</a>
-					</Link>
-				</li>
-				<li className={style.nav_item}>
-					<Link href='/contact'>
-						<a>Contact</a>
-					</Link>
-				</li>
-			</ul>
+			<div className={style.nav_items_container}>
+				<Burger open={open} setOpen={setOpen} />
+				<Menu
+					open={open}
+					currentPage={currentPage}
+					activePage={() => setOpen(!open)}
+				/>
+			</div>
 		</div>
 	)
 }
