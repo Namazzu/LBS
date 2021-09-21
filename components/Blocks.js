72965f0e4cb4cbb79b0478a419635e669cdef9a2
Fred <Fred@re450.home>
2021-09-21 19:17:28 +0200

    add real data

--- components/Blocks.js
+++ components/Blocks.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import brico1 from '../public/brico1.jpeg'
 import brico2 from '../public/brico2.jpeg'
 import brico3 from '../public/brico3.jpeg'
-import style from '../styles/Blocks.module.css'
+import style from '../styles/blocks.module.css'
 export default function Blocks() {
 	return (
 		<div className={style.blocks_container}>
@@ -11,12 +11,10 @@ export default function Blocks() {
 					<Image src={brico1} alt='picture of maker1' layout='responsive' />
 				</div>
 				<span className={style.text}>
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry's standard dummy text ever
-					since the 1500s, when an unknown printer took a galley of type and
-					scrambled it to make a type specimen book. It has survived not only
-					five centuries, but also the leap into electronic typesetting,
-					remaining essentially unchanged
+				Notre entreprise sera à votre écoute, afin de construire et réaliser avec vous vos différents
+projets
+Nous resterons à vos coté afin de vous accompagner dans les différentes étapes de vos
+Entreprises.
 				</span>
 			</div>
 			<div className={`${style.block} ${style.reverse}`}>
--- components/Presentation.js
+++ components/Presentation.js
@@ -1,18 +1,23 @@
-import style from '../styles/Presentation.module.css'
+import style from "../styles/presentation.module.css";
 export default function Presentation() {
-	return (
-		<div className={style.container}>
-			<div className={style.pres_container}>
-				<h2 className={style.title}>Bienvenue sur Les Bons Services</h2>
-				<div className={style.text}>
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry's standard dummy text ever
-					since the 1500s, when an unknown printer took a galley of type and
-					scrambled it to make a type specimen book. It has survived not only
-					five centuries, but also the leap into electronic typesetting,
-					remaining essentially unchanged.
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className={style.container}>
+      <div className={style.pres_container}>
+        <h2 className={style.title}>Bienvenue sur Les Bons Services</h2>
+        <div className={style.text}>
+          <p>
+            Les bons services est une entreprise spécialisé dans le
+            marchandising. nous remercions les clients qui nous font confiance
+            Fort de nos diverses expériences, nos équipes mettront tout en œuvre
+            pour réaliser vos différents projets.
+          </p>
+          <div className={style.citation2}>
+            <p className={style.citation}>
+              Parce que chaque partenariat commence par une rencontre.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
