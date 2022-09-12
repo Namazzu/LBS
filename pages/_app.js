2d134cfd2776d89b5bc54ca64efeb3fe3b06d80e
Namazzu <dok66300@gmail.com>
2022-09-12 20:32:52 +0200

    add google verification

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,25 @@
 import '../styles/globals.css'
 import { Navbar } from '../components/Navbar'
 import { Footer } from '../components/Footer'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Navbar />
-			<main>
-				<Component {...pageProps} />
-			</main>
-			<Footer />
-		</>
-	)
+  return (
+    <>
+      <Head>
+        <title>L.B.S</title>
+        <meta
+          name='google-site-verification'
+          content='O1Cih9CGimgvelsPoaGAvmaglP7d4aaH01EZJr_fxfE'
+        />
+      </Head>
+      <Navbar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
