27ef369e065392fcec642c96735c34ecafa37b87
Namazzu <dok66300@gmail.com>
2022-09-22 22:39:39 +0200

    add scrawler tags

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
       <Head>
         <title>LBS Accueil</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='robots' content='all' />
       </Head>
       <div className={styles.container}>
         <section>
