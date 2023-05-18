b5583846c46e3dc24161e436cf5a38db016e958a
Namazzu <dok66300@gmail.com>
2023-05-18 22:00:15 +0200

    upgrade next react react-dom and remove a into Link

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,28 +6,28 @@ import { Menu } from './Menu'
 import style from '../styles/Navbar.module.css'
 
 export function Navbar() {
-	const [open, setOpen] = useState(false)
-	const [currentPage, setCurrentPage] = useState()
+  const [open, setOpen] = useState(false)
+  const [currentPage, setCurrentPage] = useState()
 
-	useEffect(() => {
-		setCurrentPage(window.location.pathname)
-	}, [open])
+  useEffect(() => {
+    setCurrentPage(window.location.pathname)
+  }, [open])
 
-	return (
-		<div className={style.container}>
-			<h2 className={style.logo}>
-				<Link href='/'>
-					<a>LBS</a>
-				</Link>
-			</h2>
-			<div className={style.nav_items_container}>
-				<Burger open={open} setOpen={setOpen} />
-				<Menu
-					open={open}
-					currentPage={currentPage}
-					activePage={() => setOpen(!open)}
-				/>
-			</div>
-		</div>
-	)
+  return (
+    <div className={style.container}>
+      <h2 className={style.logo}>
+        <Link href='/'>
+          <div>LBS</div>
+        </Link>
+      </h2>
+      <div className={style.nav_items_container}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu
+          open={open}
+          currentPage={currentPage}
+          activePage={() => setOpen(!open)}
+        />
+      </div>
+    </div>
+  )
 }
